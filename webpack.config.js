782972e9b7cb62b855185798a76075200a029e63
Khudyakov <srt_lexus@mail.ru>
2022-05-23 18:46:04 +0500

    add unminify version and config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/app.js',
-  output: {
-    filename: 'detectUserDevice.min.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    "detectUserDevice": "./src/js/app.js",
+    "detectUserDevice.min": "./src/js/app.js",
   },
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js"
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new UglifyJsPlugin({
+      include: /\.min\.js$/
+    })]
+  }
 };
